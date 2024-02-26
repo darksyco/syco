789d8fc89f3ae64171521f57d29a6852f607c7a8
DARK <137761430+darksyco@users.noreply.github.com>
2024-02-26 16:42:28 +0530

    update

---
+++ js.js
@@ -0,0 +1,16 @@
+
+
+
+function myFunction() {
+  var x = document.getElementById("menu");
+
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  
+
+
+}
+
+}
